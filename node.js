7213c46107f42bbc448db23dd554d8c928df67f4
zujko <peter@zujko.xyz>
2018-03-05 12:05:58 -0500

    Print out election name

--- node.js
+++ node.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-var deasync = require("deasync")
-const app = express()
-const port = 6000
+const express = require('express');
+const exphbs = require('express-handlebars');
+var solc = require('solc');
+const app = express();
+const port = 3001;
 var Web3 = require('web3');
 var path = require('path');
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 var fs = require('fs');
 code = fs.readFileSync('ElectionContract.sol').toString();
-solc = require('solc');
+
 compiledCode = solc.compile(code);
 
 
@@ -17,10 +17,13 @@ abiDefinition = JSON.parse(compiledCode.contracts[':Election'].interface);
 VotingContract = web3.eth.contract(abiDefinition);
 byteCode = compiledCode.contracts[':Election'].bytecode;
 
-// The below line needs to be run synchronously else deployedContract.address is null
-deployedContract = VotingContract.new('Test Election', 10, ['Person 1','Person 2','Person 3'], {data: byteCode, from: web3.eth.accounts[0], gas: 4800000});
+var contractInstance = undefined;
+deployedContract = VotingContract.new('Test Election', 10, ['Person 1','Person 2','Person 3'], {data: byteCode, from: web3.eth.accounts[0], gas: 4800000}, function(e, res) {
+  contractInstance = VotingContract.at(res.address);
+  console.log("Got contract instance")
+});
 
-contractInstance = VotingContract.at(deployedContract.address);
+//contractInstance = VotingContract.at(deployedContract.address);
 
 app.engine('.hbs', exphbs({
     defaultLayout: 'main',
@@ -32,7 +35,7 @@ app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (request, response) => {
-  response.send('Hello from Express!')
+  response.send(contractInstance.getElectionName())
 })
 
 app.listen(port, (err) => {
